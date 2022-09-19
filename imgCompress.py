765892aa5ff5a7a5a315269d3d607ef3923b6d94
artem-tk <artem.tk24@gmail.com>
2022-09-19 16:11:35 +0300

    Nothing iteresting

--- imgCompress.py
+++ imgCompress.py
@@ -13,21 +13,27 @@ def recursiveCompress (path) :
     for i in os.listdir(path) :
 
         if (' ' in i) & ~(os.path.exists(path + '\\' + i.replace(' ',''))) : # Finding spaces and deleting them
+
             os.rename(path + '\\' + i, path + '\\' + i.replace(' ',''))
             i = i.replace(' ','')
             print(path + '\\' + i + ' was renamed')
             renamed += 1
+
         elif (' ' in i) & (os.path.exists(path + '\\' + i.replace(' ',''))) : # If file without spaces already exist - add '1_'
+
             os.rename(path + '\\' + i, path + '\\1_' + i.replace(' ',''))
             i = '1_' + i.replace(' ','')
             print(path + '\\' + i + ' was renamed')
             renamed += 1
 
         if (os.path.isdir(path + '\\' + i)) : # Recursive running all directories in path
+
             print('Go to ', (path + '\\' + i))
             recursiveCompress(path +'\\' + i)
             print('Comeback to ', path)
+
         elif (os.path.isfile(path + '\\' + i)) & ~('compressed_' in i) : # Compess images by mozjpeg ignore files with 'compressed_' in name
+            
             os.system("mozjpeg\\cjpeg -quant-table 2 -quality 75 -outfile {} {}".format(path + "\\" + "compressed_" + i, path + "\\" + i))
 
             if (os.path.exists(path + "\\" + "compressed_" + i)) : #Cheks for the existing compressed file and deleting original
